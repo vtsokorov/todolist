3f76314ff321a929bfdcbd14411172c16f1f4805
vtsokorov <vtsokorov@gmail.com>
2019-05-17 10:06:57 +0300

    ~

--- connection.php
+++ connection.php
@@ -4,8 +4,7 @@ include('mysql.php');
 class Connection
 {
 	public static function getDataBaseInstance(){
-		//return new MySqlDataBase('localhost:3306', 'root', 'root', 'todolist');
-		return new MySqlDataBase('mysql.zzz.com.ua:3306', 'freeice', 'Callisto1610', 'vyacheslavt');
+		return new MySqlDataBase('localhost:3306', 'root', '', 'todolist');
 	}
 }
 
